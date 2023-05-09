f1b7efaa7bd8481bb56a4785d297e47109f5a05a
Ruchita Gosavi <ruchigosavi10@gmail.com>
2023-05-09 19:31:37 +0530

    refactor: change content and bg image of Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,12 @@
+import styles from '../styles/Home.module.css';
+
 const Home = () => {
-  return <div>Shop now</div>;
+  return (
+    <div className={styles.home}>
+      <p className={styles.tagline}>Step into adventure with</p>
+      <p className={styles.tagline}>every stride</p>
+    </div>
+  );
 };
 
 export default Home;
